512f2fff4fde455285c78c2d50b99053aad9a180
Sal Palacios <salpalacios@3dresults.com>
2018-07-31 14:07:45 -0500

    test

--- DappUniversity_BuildOwnCrypto/test/DappToken.js
+++ DappUniversity_BuildOwnCrypto/test/DappToken.js
@@ -2,16 +2,19 @@ var DappToken = artifacts.require("./DappToken.sol")
 
 contract("Dapptoken", function(accounts){
 
-   it('sets the total supply on deployment')
-   {
-    return DappToken.deployed()
-            .then(function(instance){
-                var tokenInstance = instance;
-                return tokenInstance.totalSupply();
-            })
-            .then(function(totalSupply){
-                assert.equal(totalSupply.toNumber(),1000000,'Test total supply to 1,000,000');
-            })
-       
-   };
+    it('sets the total supply on deployment',function(){
+
+        return DappToken.deployed()
+        .then(function(instance)
+        { 
+            tokenInstance = instance;
+            return tokenInstance.totalSupply();
+        })
+        .then(function (totalSupply)
+        {
+            assert.equal(totalSupply.toNumber(),5000000,'Test total supply to 1,000,000');
+        })
+
+    })
+      
 })
