23f2591dd5dd3c71c9d03c6dc6ac001074f121bc
Sal Palacios <salpalacios@3dresults.com>
2018-08-01 15:18:38 -0500

    adding more unit tests for contract

--- DappUniversity_BuildOwnCrypto/contracts/DappToken.sol
+++ DappUniversity_BuildOwnCrypto/contracts/DappToken.sol
@@ -1,19 +1,35 @@
 pragma solidity ^0.4.2;
 
 contract DappToken{
-    // how many tokens 
-
+     
     // constructor
 
     //  Public state variable gets writtent to blockchain
     uint256 public totalSupply;
+    // total Supply Mapping
+    mapping(address => uint256) public balanceOf;
 
-    constructor()  public {
-        totalSupply = 1000000;
+    // Create token name
+    string public name = "DappToken";
 
+    // Create token symbol
+    string public symbol = "DAPP";
+
+    // Create token symbol
+    string public standard = "Dapp Token v.1.0";
+
+
+    constructor(uint256 _initialSupply)  public {
+        // Balance of Admin (msg.sender is first account in Ganache)
+        // msg is built in object that stores globals sent to contrac t
+        balanceOf[msg.sender] = _initialSupply;
+       
+        totalSupply = _initialSupply;
+        //allocate the initial supply
     }
 
     
     // Set the tokens
+
     // Rea the toal number of token
 }
--- DappUniversity_BuildOwnCrypto/test/DappToken.js
+++ DappUniversity_BuildOwnCrypto/test/DappToken.js
@@ -1,20 +1,48 @@
 var DappToken = artifacts.require("./DappToken.sol")
 
-contract("Dapptoken", function(accounts){
-
-    it('sets the total supply on deployment',function(){
-
+contract("DappToken", function(accounts){ 
+    it('Initializes the contract with correct value',function(){
       return DappToken.deployed()
-        .then(function(instance)
-        { 
-            tokenInstance = instance;
-            return tokenInstance.totalSupply();
-        })
-        .then(function (totalSupply)
-        {
-            assert.equal(totalSupply.toNumber(),5000000,'Test total supply to 1,000,000');
-        })
+
+              .then(function(instance){
+                tokenInstance = instance; 
+                return tokenInstance.name();
+              })
+
+              .then(function(name){
+                assert.equal(name,'DappToken','Token has a name') ;
+                return tokenInstance.symbol();
+              })
+
+              .then(function(symbol){
+                assert.equal(symbol,'DAPP','Token has a symboo') ;
+                 return tokenInstance.standard();
+              }) 
+
+              .then(function(standard){
+                 assert.equal(standard,'Dapp Token v.1.0')
+                
+              })
 
     })
-      
+
+    it('Sets the total supply on deployment',function(){ 
+
+      const InitBalance = 1000000; 
+      return DappToken.deployed()
+                .then(function(instance)
+                { 
+                    tokenInstance = instance;
+                    return tokenInstance.totalSupply();
+                }) 
+                .then(function (totalSupply)
+                {
+                    // Ganache uses first account accont[0] as admin account
+                    assert.equal( totalSupply.toNumber(), InitBalance,'Test total supply to 1,000,000');
+                    return tokenInstance.balanceOf(accounts[0]);
+                }) 
+                .then (function(adminBalance){
+                    assert.equal(adminBalance.toNumber(),InitBalance, 'it allocates the initial supply');
+                })
+    }) 
 })
